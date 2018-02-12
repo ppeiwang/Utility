249bfb0e6d37ece54e4cd829007923243d66695f
ppwang <jobwangpei@gmail.com>
2018-02-12 17:41:35 +0800

    add string tokenlizor

--- String/PPCString.h
+++ String/PPCString.h
@@ -1,18 +1,31 @@
 #pragma once
+#include <vector>
 #include <string>
 
 namespace PEIPEISPACE
 {
-	void trim(std::string &s, const std::string& strTarget)
+	// string for ANSI
+	// wstring for unicode & multibyte
+
+	/*
+	trimLeft:
+		std::string strSrc = "abcdabc";
+		e.g. trimLeft(strSrc, "a b c e");
+		std::cout<<strSrc; // will print "dabc", every character that is in pattern "a b c e" will be deleted
+	*/
+
+	template<class T>
+	void trim(T& s, const T& strTarget)
 	{
 		if (s.empty() == false)
 		{
 			s.erase(0, s.find_first_not_of(strTarget));
-			s.erase(s.find_last_not_of(" ") + 1);
+			s.erase(s.find_last_not_of(strTarget) + 1);
 		}
 	}
 
-	void trimLeft(std::string &s, const std::string& strTarget)
+	template<class T>
+	void trimLeft(T &s, const T& strTarget)
 	{
 		if (s.empty() == false)
 		{
@@ -20,11 +33,48 @@ namespace PEIPEISPACE
 		}
 	}
 
-	void trimRight(std::string &s, const std::string& strTarget)
+	template<class T>
+	void trimRight(T& s, const T& strTarget)
 	{
 		if (s.empty() == false)
 		{
 			s.erase(s.find_last_not_of(strTarget) + 1);
 		}
 	}
+
+
+	template<class T>
+	void StringTokenizer(std::vector<T>& _tokens, const T& str, const T& separators, bool bIgnoreEmpty = true)
+	{
+		typename T::const_iterator it = str.begin();
+		typename T::const_iterator end = str.end();
+
+		T token;
+		bool lastToken = false;
+
+		for (; it != end; ++it)
+		{
+			if (separators.find(*it) != T::npos)
+			{
+				if (!token.empty() || !bIgnoreEmpty) _tokens.push_back(token);
+				if (!bIgnoreEmpty) lastToken = true;
+				token.clear();
+			}
+			else
+			{
+				token += *it;
+				lastToken = false;
+			}
+		}
+
+		if (!token.empty())
+		{
+			if (!token.empty() || !bIgnoreEmpty) _tokens.push_back(token);
+		}
+		else if (lastToken)
+		{
+			_tokens.push_back(T());
+		}
+	}
+	
 }
